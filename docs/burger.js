6e1d2e62297a9841cf6a221630598972d967f0df
tatsu <tatsukichisan@protonmail.com>
2023-11-24 19:22:14 +0900

    Update burger.js

--- docs/burger.js
+++ docs/burger.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Add a click event on each of them
+  if ($navbarBurgers.length > 0) {
+
     $navbarBurgers.forEach( el => {
       el.addEventListener('click', () => {
-  
-        // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
-  
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
-  
+      });
+
+      // Attach click event to navbar items
+      const $navItems = Array.prototype.slice.call(document.querySelectorAll('.navbar-item'), 0);
+
+      $navItems.forEach(item => {
+          item.addEventListener('click', () => {
+              // Remove 'is-active' class from burger and menu
+              el.classList.remove('is-active');
+              document.getElementById(el.dataset.target).classList.remove('is-active');
+          });
       });
     });
-  
-  });
+  }
+});
